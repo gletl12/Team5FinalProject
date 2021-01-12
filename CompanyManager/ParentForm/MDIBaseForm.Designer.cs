3ee20c7b43229b89f7c5f2c874da519d4dcbcbf7
rudgns <rudgns0512@naver.com>
2021-01-12 16:35:42 +0900

    base

--- CompanyManager/ParentForm/MDIBaseForm.Designer.cs
+++ CompanyManager/ParentForm/MDIBaseForm.Designer.cs
@@ -29,10 +29,18 @@ namespace CompanyManager
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MDIBaseForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1092, 609);
+            this.Font = new System.Drawing.Font("나눔고딕", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.Name = "MDIBaseForm";
             this.Text = "MDIBaseForm";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
