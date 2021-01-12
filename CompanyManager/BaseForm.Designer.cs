54905251f4070eae2d2459acd7bb560f4cbe8598
rudgns <rudgns0512@naver.com>
2021-01-12 15:36:17 +0900

    base

--- CompanyManager/BaseForm.Designer.cs
+++ CompanyManager/BaseForm.Designer.cs
@@ -35,7 +35,7 @@ namespace CompanyManager
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1092, 710);
+            this.ClientSize = new System.Drawing.Size(1092, 609);
             this.Font = new System.Drawing.Font("나눔고딕", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.Name = "BaseForm";
             this.Text = "BaseForm";
