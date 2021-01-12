7aa45e4ca8da598d4ae0c7aea27e6b178cc2f243
이진식 <wlstlr1108@naver.com>
2021-01-12 17:24:09 +0900

    22

---
+++ API/Controllers/SampleController.cs
@@ -0,0 +1,61 @@
+﻿using DAC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using VO;
+
+namespace API.Controllers
+{
+    public class SampleController : ApiController
+    {
+        // GET: api/Sample
+        public List<MenuVO> Get()
+        {
+            MenuDAC dac = new MenuDAC();
+            return dac.GetMenus();
+        }
+
+        // GET: api/Sample/5
+        // Example : https://localhost:44384/api/Sample/5
+        public MenuVO Get(int id)
+        {
+            MenuDAC dac = new MenuDAC();
+            return dac.GetMenus()[id];
+        }
+
+        // POST: api/Sample
+        public HttpResponseMessage Post([FromBody]MenuVO value)
+        {
+            //DAC단에서 추가 메서드 실행후 결과에 따라 HttpResponseMessage형식으로 반환
+
+            if (2 == 1)
+            {
+                HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.Created, value.FormName);
+                response.Content = new StringContent($"메시지 입력", System.Text.Encoding.Unicode);
+
+                return response;
+            }
+            else
+            {
+                return new HttpResponseMessage(HttpStatusCode.NoContent);
+            }
+
+
+
+        }
+
+        // PUT: api/Sample/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Sample/5
+        public void Delete(int id)
+        {
+
+        }
+    }
+}
