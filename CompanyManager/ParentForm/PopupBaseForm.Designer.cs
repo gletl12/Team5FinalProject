5c65f40465e1d886caf9c5b2fd0b7427f1738ecf
rudgns <rudgns0512@naver.com>
2021-01-12 17:50:59 +0900

    test

--- CompanyManager/ParentForm/PopupBaseForm.Designer.cs
+++ CompanyManager/ParentForm/PopupBaseForm.Designer.cs
@@ -54,8 +54,6 @@ namespace CompanyManager
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "PopupBaseForm";
             this.Text = "PopupBaseForm";
-            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.PopupBaseForm_FormClosed);
-            this.Load += new System.EventHandler(this.PopupBaseForm_Load);
             this.ResumeLayout(false);
 
         }
