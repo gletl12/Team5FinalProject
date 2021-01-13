59218958bdbe1e53564c7ac11d4f80a671f6059b
rudgns <rudgns0512@naver.com>
2021-01-13 17:47:01 +0900

    mdi size

--- CompanyManager/ParentForm/MDIBaseForm.Designer.cs
+++ CompanyManager/ParentForm/MDIBaseForm.Designer.cs
@@ -35,7 +35,7 @@ namespace CompanyManager
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1079, 577);
+            this.ClientSize = new System.Drawing.Size(1169, 646);
             this.Font = new System.Drawing.Font("나눔고딕", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "MDIBaseForm";
