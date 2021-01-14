5fc32481f6b48d7a1a4841e793c51e93a0145d17
rudgns <rudgns0512@naver.com>
2021-01-14 17:23:33 +0900

    teae

--- CompanyManager/ParentForm/MDIBaseForm.Designer.cs
+++ CompanyManager/ParentForm/MDIBaseForm.Designer.cs
@@ -88,7 +88,6 @@ namespace CompanyManager
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1148, 98);
             this.panel1.TabIndex = 2;
-            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // label9
             // 
