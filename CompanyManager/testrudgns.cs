ab1c9d7a1fcafb3855836505cd4ae34b0a0bd6f8
rudgns <rudgns0512@naver.com>
2021-01-18 16:19:35 +0900

    test

---
+++ CompanyManager/testrudgns.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CompanyManager
+{
+    public partial class testrudgns : Form
+    {
+        public testrudgns()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void testrudgns_Load(object sender, EventArgs e)
+        {
+            panel1.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (panel1.Visible == true)
+                panel1.Visible = false;
+            else
+                panel1.Visible = true;
+        }
+    }
+}
