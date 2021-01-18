9a210fb02acb469a12278cf16867b8bffc53d191
rudgns <rudgns0512@naver.com>
2021-01-18 15:30:02 +0900

    ttt

---
+++ CompanyManager/Sample/MDISampleControl2.cs
@@ -0,0 +1,30 @@
+﻿using Service;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Util;
+
+namespace CompanyManager.Sample
+{
+    public partial class MDISampleControl2 : CompanyManager.MDIBaseForm
+    {
+        public MDISampleControl2()
+        {
+            InitializeComponent();
+        }
+
+        private void MDISampleControl2_Load(object sender, EventArgs e)
+        {
+            CommonUtil.SetDGVDesign(dataGridView2);
+            MenuService service = new MenuService();
+            dataGridView2.DataSource = service.GetMenus();
+
+            CommonUtil.SetDGVDesign(dataGridView1);
+            dataGridView1.DataSource = service.GetMenus();
+        }
+    }
+}
