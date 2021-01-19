a60eca5d23ffaebee4ff9dce365c1c9aa1079ae7
rudgns <rudgns0512@naver.com>
2021-01-19 17:16:33 +0900

    POP

---
+++ POP/FrmAction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace POP
+{
+    public partial class FrmAction : POP.BaseForm
+    {
+        public FrmAction()
+        {
+            InitializeComponent();
+        }
+    }
+}
