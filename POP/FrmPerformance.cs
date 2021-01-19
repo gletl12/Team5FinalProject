d06f1a838efc41c632aba202620690fe7d836b38
rudgns <rudgns0512@naver.com>
2021-01-19 17:20:27 +0900

    pop 화면

---
+++ POP/FrmPerformance.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace POP
+{
+    public partial class FrmPerformance : POP.BaseForm
+    {
+        public FrmPerformance()
+        {
+            InitializeComponent();
+        }
+    }
+}
