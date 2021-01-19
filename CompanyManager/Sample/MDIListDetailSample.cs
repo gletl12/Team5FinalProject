d52a4a8c0aaaf2978c10b5c8839d2d8794b126ef
rudgns <rudgns0512@naver.com>
2021-01-19 16:47:01 +0900

    그리드뷰 행번호 수정

--- CompanyManager/Sample/MDIListDetailSample.cs
+++ CompanyManager/Sample/MDIListDetailSample.cs
@@ -55,5 +55,56 @@ namespace CompanyManager
 
             }
         }
+
+        private void dataGridView1_RowPostPaint(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            dataGridView1.RowHeadersWidth = 50;
+            dataGridView1.RowHeadersVisible = true;
+            StringFormat drawFormat = new StringFormat();
+            //drawFormat.FormatFlags = StringFormatFlags.DirectionVertical;
+            drawFormat.FormatFlags = StringFormatFlags.DirectionRightToLeft;
+
+            using (Brush brush = new SolidBrush(Color.Black))
+            {
+                e.Graphics.DrawString((e.RowIndex + 1).ToString(), e.InheritedRowStyle.Font,
+                brush, e.RowBounds.Location.X + 35, e.RowBounds.Location.Y + 4, drawFormat);
+            }
+        }
+
+        private void dataGridView1_CurrentCellDirtyStateChanged(object sender, EventArgs e)
+        {
+            if (dataGridView1.CurrentCell is DataGridViewCheckBoxCell)
+            {
+
+                dataGridView1.CommitEdit(DataGridViewDataErrorContexts.Commit);
+
+            }
+        }
+
+        private void dataGridView2_CurrentCellDirtyStateChanged_1(object sender, EventArgs e)
+        {
+            if (dataGridView2.CurrentCell is DataGridViewCheckBoxCell)
+            {
+
+                dataGridView2.CommitEdit(DataGridViewDataErrorContexts.Commit);
+
+            }
+        }
+
+        private void dataGridView2_RowPostPaint_1(object sender, DataGridViewRowPostPaintEventArgs e)
+        {
+            dataGridView2.RowHeadersWidth = 50;
+            dataGridView2.RowHeadersVisible = true;
+            StringFormat drawFormat = new StringFormat();
+            //drawFormat.FormatFlags = StringFormatFlags.DirectionVertical;
+            drawFormat.FormatFlags = StringFormatFlags.DirectionRightToLeft;
+
+            using (Brush brush = new SolidBrush(Color.Black))
+            {
+                e.Graphics.DrawString((e.RowIndex + 1).ToString(), e.InheritedRowStyle.Font,
+                brush, e.RowBounds.Location.X + 35, e.RowBounds.Location.Y + 4, drawFormat);
+            }
+        }
+
     }
 }
