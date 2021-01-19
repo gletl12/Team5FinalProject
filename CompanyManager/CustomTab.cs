99f7d839596f4713899d260e6240504768483487
이진식 <wlstlr1108@naver.com>
2021-01-19 14:58:42 +0900

    tab

--- CompanyManager/CustomTab.cs
+++ CompanyManager/CustomTab.cs
@@ -15,6 +15,53 @@ namespace CompanyManager
         public CustomTab()
         {
             InitializeComponent();
+
+            this.DrawMode = TabDrawMode.OwnerDrawFixed;
+        }
+
+        protected override void OnDrawItem(DrawItemEventArgs e)
+        {
+            base.OnDrawItem(e);
+            
+            try
+            {
+                Rectangle r = e.Bounds;
+                r = this.GetTabRect(e.Index);
+                r.Offset(2,2);
+
+                //선택된 탭의 백그라운드색상은 흰색 (나머지 탭은 기본값)
+                if (this.SelectedIndex == e.Index)
+                {
+                    e.Graphics.FillRectangle(new SolidBrush(Color.FromArgb(233, 236, 239)), e.Bounds);
+                }
+                else
+                {
+                    e.Graphics.FillRectangle(new SolidBrush(Color.White), e.Bounds);
+                }
+
+                //탭의 글씨
+                Brush titleBrush;
+                string title = this.TabPages[e.Index].Text;
+                Font f = this.Font;
+
+                if (this.SelectedIndex == e.Index)
+                {
+                   titleBrush = new SolidBrush(Color.Black);
+                }
+                else
+                {
+                    titleBrush = new SolidBrush(Color.Black);
+                }
+
+                
+                e.Graphics.DrawString(title, f, titleBrush, new Point(r.X, r.Y));
+
+            }
+            catch
+            {
+
+            }
+            
         }
 
         protected override void OnPaint(PaintEventArgs pe)
