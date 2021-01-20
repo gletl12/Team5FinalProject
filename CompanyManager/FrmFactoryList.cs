cb96f2401d9a7c72f2a46c159b7825a24d5e286d
준호 정 <todaywls15@naver.com>
2021-01-20 21:41:30 +0900

    공장 리스트 커밋

---
+++ CompanyManager/FrmFactoryList.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Util;
+
+namespace CompanyManager
+{
+    public partial class FrmFactoryList : CompanyManager.MDIBaseForm
+    {
+        public FrmFactoryList()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmFactoryList_Load(object sender, EventArgs e)
+        {
+            GetdgvColumn();
+        }
+
+        private void GetdgvColumn()
+        {
+            CommonUtil.SetDGVDesign(dgvFactory);
+
+            CommonUtil.AddGridTextColumn(dgvFactory, "No.", "no");
+            CommonUtil.AddGridCheckColumn(dgvFactory, "");
+            CommonUtil.AddGridCheckColumn(dgvFactory, "No.");
+            CommonUtil.AddGridTextColumn(dgvFactory, "No.", "no");
+            CommonUtil.AddGridTextColumn(dgvFactory, "No.", "no");
+            CommonUtil.AddGridTextColumn(dgvFactory, "No.", "no");
+            CommonUtil.AddGridTextColumn(dgvFactory, "No.", "no");
+        }
+    }
+}
