be0779bb0fec96661b648ed0284da7cb554a9c74
choigeon96@gmail.com <choigeon96@gmail.com>
2021-01-20 21:49:47 +0900

    화면설계

---
+++ CompanyManager/FrmAwait.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CompanyManager
+{
+    public partial class FrmAwait : CompanyManager.MDIBaseForm
+    {
+        public FrmAwait()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
