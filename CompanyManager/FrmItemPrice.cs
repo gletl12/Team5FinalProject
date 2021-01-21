3b52090d2cd006d2d92204260346c33152535ee6
choigeon96@gmail.com <choigeon96@gmail.com>
2021-01-21 15:12:35 +0900

    화면설계

--- CompanyManager/FrmItemPrice.cs
+++ CompanyManager/FrmItemPrice.cs
@@ -17,7 +17,7 @@ namespace CompanyManager
             CommonUtil.SetInitGridView(dataGridView2);
             CommonUtil.SetDGVDesign_Num(dataGridView2);
             CommonUtil.AddGridCheckColumn(dataGridView2,"");
-            CommonUtil.AddGridImageColumn(dataGridView2, Properties.Resources.Edit_16x16, "Edit",20);
+            CommonUtil.AddGridImageColumn(dataGridView2, Properties.Resources.Edit_16x16, "Edit",40);
             CommonUtil.AddGridTextColumn(dataGridView2, "업체", "CompanyName",80);
             CommonUtil.AddGridTextColumn(dataGridView2, "업체명", "CompanyName",100);
             CommonUtil.AddGridTextColumn(dataGridView2, "품목", "CompanyName",100);
@@ -32,6 +32,11 @@ namespace CompanyManager
             CommonUtil.AddGridTextColumn(dataGridView2, "종료일", "CompanyName",80);
             CommonUtil.AddGridTextColumn(dataGridView2, "비고", "CompanyName",120);
             CommonUtil.AddGridTextColumn(dataGridView2, "사용유무", "CompanyName",80);
+
+            checkBox1.Location = new Point(dataGridView2.Location.X + 54, dataGridView2.Location.Y + 5);
+
+            dataGridView2.Rows.Add(null, null, "GD", "구디", "L_WOOD_04", "의자 다리 원목", "500 * 200", "갯수", "내수", "KRW", "700.00", "650.00", "2021-01-01", "9999-01-01");
+            dataGridView2.Rows.Add(null, null, "OS", "(주)에이더블유", "BACK_A_02", "등받이", "5 * 12 inch", "갯수", "내수", "KRW", "200.00", "0", "2021-01-01", "9999-01-01");
         }
     }
 }
