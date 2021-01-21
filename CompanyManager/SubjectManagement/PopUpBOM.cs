2809eccbca9fe48a82700caf9be17456d4a9b9ea
이진식 <wlstlr1108@naver.com>
2021-01-21 16:33:09 +0900

    tt

---
+++ CompanyManager/SubjectManagement/PopUpBOM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CompanyManager
+{
+    public partial class PopUpBOM : CompanyManager.PopupBaseForm
+    {
+        public PopUpBOM()
+        {
+            InitializeComponent();
+        }
+
+        private void PopUpBOM_Load(object sender, EventArgs e)
+        {
+            popupTitleBar1.HeaderText = "BOM";
+        }
+    }
+}
