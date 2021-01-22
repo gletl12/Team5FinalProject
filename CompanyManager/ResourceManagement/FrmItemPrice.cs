5148bf59bcbcb23ee13710aca849c614f6147248
choigeon96@gmail.com <choigeon96@gmail.com>
2021-01-22 17:50:14 +0900

    자재단가관리

--- CompanyManager/ResourceManagement/FrmItemPrice.cs
+++ CompanyManager/ResourceManagement/FrmItemPrice.cs
@@ -14,29 +14,63 @@ namespace CompanyManager
         public FrmItemPrice()
         {
             InitializeComponent();
-            CommonUtil.SetInitGridView(dataGridView2);
-            CommonUtil.SetDGVDesign_Num(dataGridView2);
-            CommonUtil.AddGridCheckColumn(dataGridView2,"");
-            CommonUtil.AddGridImageColumn(dataGridView2, Properties.Resources.Edit_16x16, "Edit",40);
-            CommonUtil.AddGridTextColumn(dataGridView2, "업체", "CompanyName",80);
-            CommonUtil.AddGridTextColumn(dataGridView2, "업체명", "CompanyName",100);
-            CommonUtil.AddGridTextColumn(dataGridView2, "품목", "CompanyName",100);
-            CommonUtil.AddGridTextColumn(dataGridView2, "품명", "CompanyName",120);
-            CommonUtil.AddGridTextColumn(dataGridView2, "규격", "CompanyName",100);
-            CommonUtil.AddGridTextColumn(dataGridView2, "단위", "CompanyName",80);
-            CommonUtil.AddGridTextColumn(dataGridView2, "Market", "CompanyName",80);
-            CommonUtil.AddGridTextColumn(dataGridView2, "환종", "CompanyName",60);
-            CommonUtil.AddGridTextColumn(dataGridView2, "현재단가", "CompanyName",80);
-            CommonUtil.AddGridTextColumn(dataGridView2, "이전단가", "CompanyName",80);
-            CommonUtil.AddGridTextColumn(dataGridView2, "시작일", "CompanyName",80);
-            CommonUtil.AddGridTextColumn(dataGridView2, "종료일", "CompanyName",80);
-            CommonUtil.AddGridTextColumn(dataGridView2, "비고", "CompanyName",120);
-            CommonUtil.AddGridTextColumn(dataGridView2, "사용유무", "CompanyName",80);
+            CommonUtil.SetInitGridView(dgvPrice);
+            CommonUtil.SetDGVDesign_Num(dgvPrice);
+            CommonUtil.AddGridCheckColumn(dgvPrice, "");
+            CommonUtil.AddGridImageColumn(dgvPrice, Properties.Resources.Edit_16x16, "Edit", 40);
+            CommonUtil.AddGridTextColumn(dgvPrice, "업체", "CompanyName", 80);
+            CommonUtil.AddGridTextColumn(dgvPrice, "업체명", "CompanyName", 100);
+            CommonUtil.AddGridTextColumn(dgvPrice, "품목", "CompanyName", 100);
+            CommonUtil.AddGridTextColumn(dgvPrice, "품명", "CompanyName", 120);
+            CommonUtil.AddGridTextColumn(dgvPrice, "규격", "CompanyName", 100);
+            CommonUtil.AddGridTextColumn(dgvPrice, "단위", "CompanyName", 80);
+            CommonUtil.AddGridTextColumn(dgvPrice, "Market", "CompanyName", 80);
+            CommonUtil.AddGridTextColumn(dgvPrice, "환종", "CompanyName", 60);
+            CommonUtil.AddGridTextColumn(dgvPrice, "현재단가", "CompanyName", 80);
+            CommonUtil.AddGridTextColumn(dgvPrice, "이전단가", "CompanyName", 80);
+            CommonUtil.AddGridTextColumn(dgvPrice, "시작일", "CompanyName", 80);
+            CommonUtil.AddGridTextColumn(dgvPrice, "종료일", "CompanyName", 80);
+            CommonUtil.AddGridTextColumn(dgvPrice, "비고", "CompanyName", 120);
+            CommonUtil.AddGridTextColumn(dgvPrice, "사용유무", "CompanyName", 80);
 
-            checkBox1.Location = new Point(dataGridView2.Location.X + 54, dataGridView2.Location.Y + 5);
+            CommonUtil.AddGridTextColumn(dgvPrice, "업체", "CompanyName", 80);
+            CommonUtil.AddGridTextColumn(dgvPrice, "업체명", "CompanyName", 100);
+            CommonUtil.AddGridTextColumn(dgvPrice, "품목", "CompanyName", 100);
+            CommonUtil.AddGridTextColumn(dgvPrice, "품명", "CompanyName", 120);
+            CommonUtil.AddGridTextColumn(dgvPrice, "규격", "CompanyName", 100);
+            CommonUtil.AddGridTextColumn(dgvPrice, "단위", "CompanyName", 80);
+            CommonUtil.AddGridTextColumn(dgvPrice, "Market", "CompanyName", 80);
+            CommonUtil.AddGridTextColumn(dgvPrice, "환종", "CompanyName", 60);
+            CommonUtil.AddGridTextColumn(dgvPrice, "현재단가", "CompanyName", 80);
+            CommonUtil.AddGridTextColumn(dgvPrice, "이전단가", "CompanyName", 80);
+            CommonUtil.AddGridTextColumn(dgvPrice, "시작일", "CompanyName", 80);
+            CommonUtil.AddGridTextColumn(dgvPrice, "종료일", "CompanyName", 80);
+            CommonUtil.AddGridTextColumn(dgvPrice, "비고", "CompanyName", 120);
+            CommonUtil.AddGridTextColumn(dgvPrice, "사용유무", "CompanyName", 80);
 
-            dataGridView2.Rows.Add(null, null, "GD", "구디", "L_WOOD_04", "의자 다리 원목", "500 * 200", "갯수", "내수", "KRW", "700.00", "650.00", "2021-01-01", "9999-01-01");
-            dataGridView2.Rows.Add(null, null, "OS", "(주)에이더블유", "BACK_A_02", "등받이", "5 * 12 inch", "갯수", "내수", "KRW", "200.00", "0", "2021-01-01", "9999-01-01");
+            checkBox1.Location = new Point(dgvPrice.Location.X + 54, dgvPrice.Location.Y + 5);
+            dgvPrice.Scroll += DgvPrice_Scroll;
+            dgvPrice.CellClick += DgvPrice_CellClick;
+            dgvPrice.Rows.Add(null, null, "GD", "구디", "L_WOOD_04", "의자 다리 원목", "500 * 200", "갯수", "내수", "KRW", "700.00", "650.00", "2021-01-01", "9999-01-01");
+            dgvPrice.Rows.Add(null, null, "OS", "(주)에이더블유", "BACK_A_02", "등받이", "5 * 12 inch", "갯수", "내수", "KRW", "200.00", "0", "2021-01-01", "9999-01-01");
+
+        }
+
+        private void DgvPrice_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            MessageBox.Show($"{e.RowIndex},{e.ColumnIndex}");
+            MessageBox.Show(dgvPrice[-1, -1].GetType().ToString());
+        }
+
+        private void DgvPrice_Scroll(object sender, ScrollEventArgs e)
+        {
+            if (e.ScrollOrientation == ScrollOrientation.HorizontalScroll)
+            {
+                label1.Text = e.Type.ToString();
+                label2.Text = (e.OldValue - e.NewValue).ToString();
+                label4.Text = e.ScrollOrientation.ToString();
+                checkBox1.Location = new Point(checkBox1.Location.X - (e.NewValue - e.OldValue), checkBox1.Location.Y);
+            }
         }
     }
 }
