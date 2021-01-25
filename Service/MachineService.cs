81907c8daa2659ba4075f431f07e707cb92d3f35
rudgns <rudgns0512@naver.com>
2021-01-25 15:00:25 +0900

    머신 서비스

---
+++ Service/MachineService.cs
@@ -0,0 +1,22 @@
+﻿using DAC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VO;
+
+namespace Service
+{
+   public class MachineService
+    {
+        public class ShiftService
+        {
+            public List<MachineVO> GetMachine()
+            {
+                MachineDAC dac = new MachineDAC();
+                return dac.GetMachine();
+            }
+        }
+    }
+}
