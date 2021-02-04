7dc012b7cafa40758ff134f5f2cbf2fad768b61a
rudgns <rudgns0512@naver.com>
2021-02-04 15:15:44 +0900

    ssss

---
+++ Service/WorkOrderService.cs
@@ -0,0 +1,20 @@
+﻿using DAC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VO;
+
+namespace Service
+{
+    public class WorkOrderService
+    {
+
+        public List<WorkOrderVO> GetWorkOrder()
+        {
+            WorkOrderDAC dac = new WorkOrderDAC();
+            return dac.GetWorkOrder();
+        }
+    }
+}
