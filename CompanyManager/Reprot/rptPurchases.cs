a975e6c9201a2455bef80cd268a35076994b9d73
choigeon96@gmail.com <choigeon96@gmail.com>
2021-02-16 15:02:55 +0900

    발주서

---
+++ CompanyManager/Reprot/rptPurchases.cs
@@ -0,0 +1,17 @@
+﻿using DevExpress.XtraReports.UI;
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Drawing;
+
+namespace CompanyManager
+{
+    public partial class rptPurchases : DevExpress.XtraReports.UI.XtraReport
+    {
+        public rptPurchases()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
